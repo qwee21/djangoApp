da8dd67050d30c4f7ee5c7ff3ed246975d3b831f
Ilya Barashkov <rete234@mail.ru>
2024-05-28 22:47:11 +0300

    firts v pagination

--- app1/goods/views.py
+++ app1/goods/views.py
@@ -2,25 +2,25 @@ from django.shortcuts import render
 
 from goods.models import Products
 
-def catalog(request):
 
-    goods = Products.objects.all()
+def catalog(request, category_slug):
 
-
+    if category_slug == "all":
+        goods = Products.objects.all()
+    else:
+        goods = Products.objects.filter(category__slug=category_slug)
 
     context = {
         "title": "Home - Каталог",
-        "goods": goods, 
+        "goods": goods,
     }
     return render(request, "goods/catalog.html", context)
 
 
-def product(request, product_id):
+def product(request, product_slug):
 
-    product = Products.objects.get(id=product_id)
+    product = Products.objects.get(slug=product_slug)
 
-    context = {
-        'product': product
-    }
+    context = {"product": product}
 
     return render(request, "goods/product.html", context=context)
