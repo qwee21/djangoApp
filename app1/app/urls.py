bedb7bedd2cf8dc1de97c03599ae6436b9624267
Ilya Barashkov <rete234@mail.ru>
2024-05-29 19:08:49 +0300

    Templates for users app added

--- app1/app/urls.py
+++ app1/app/urls.py
@@ -28,7 +28,9 @@ from app import settings
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("main.urls", namespace="main")),
-    path("catalog/", include("goods.urls", namespace="catalog"))
+    path("catalog/", include("goods.urls", namespace="catalog")),
+    path('user/', include('users.urls', namespace='user'))
+
 ]
 if settings.DEBUG:
     urlpatterns += [path("__debug__/", include("debug_toolbar.urls"))]
